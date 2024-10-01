408327d8748b2834bbb738fd612ffa03aa945e46
bakkavamshi0171 <118384205+bakkavamshi0171@users.noreply.github.com>
2024-10-01 11:54:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function introduce(greeting, location){
+    console.log(greeting + " this is " + this.name + " i am from " + location );
+
+}
+let person1= {
+    name : "vamshi"
+}
+introduce.call(person1, "Hello", "Hyderabad");
+
+function introduce(greeting, location){
+    console.log(greeting + " this is " + this.name + " i am from " + location );
+}
+let person2= {
+    name : "vamshi",
+}
+introduce.apply(person2, ["Hello", "Hyderabad"]);
+
+
+
+function introduce(greeting, location){
+    console.log(greeting + " this is " + this.name + " i am from " + location );
+
+}
+let person3= {
+    name : "vamshi"
+}
+let bind = introduce.bind(person3, "Hello", "Hyderabad" );
+bind();
